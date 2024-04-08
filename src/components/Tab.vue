65c63dddac0d7eedbb8fd41c3b92811fd491f836
Ben // Zerofour // 04 <ho.ben2004@gmail.com>
2024-04-08 23:04:13 +0200

    Initial commit

---
+++ src/components/Tab.vue
@@ -0,0 +1,15 @@
+<template>
+    <FormKit type="form">
+        <FormKit
+        type="text"
+        name="name"
+        id="name"
+        label="Name"
+        help="Your full name"a
+        placeholder="“Jon Doe”"
+    />
+    </FormKit>
+    <p>Test</p>
+</template>
+
+
